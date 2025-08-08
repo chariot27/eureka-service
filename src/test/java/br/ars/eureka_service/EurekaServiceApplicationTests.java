bfa5f7c2393c259db51f9a5f57d3538507445354
chariot27 <maxtarigodev@gmail.com>
2025-08-08 12:23:11 -0300

    eureka-service

---
+++ src/test/java/br/ars/eureka_service/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ars.eureka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
